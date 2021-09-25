dc718f8a6b23f659fcd519ef5faa8efd6306ab0d
Gustavo Palmeira <gustavo_palmeira07@hotmail.com>
2021-09-24 22:51:41 -0300

    NavBottom + NavView

--- app/src/main/java/br/senac/aula07examples/bottomnavigation/BottomNavigationActivity.kt
+++ app/src/main/java/br/senac/aula07examples/bottomnavigation/BottomNavigationActivity.kt
@@ -2,7 +2,12 @@ package br.senac.aula07examples.bottomnavigation
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import br.senac.aula07examples.R
 import br.senac.aula07examples.databinding.ActivityBottomNavigationBinding
+import br.senac.aula07examples.fragments.AlbumsFragment
+import br.senac.aula07examples.fragments.ArtistsFragment
+import br.senac.aula07examples.fragments.RecentsFragment
 
 class BottomNavigationActivity : AppCompatActivity() {
 
@@ -13,6 +18,36 @@ class BottomNavigationActivity : AppCompatActivity() {
         binding = ActivityBottomNavigationBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        var frag: Fragment = RecentsFragment.newInstance()
+
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.container, frag).commit()
+
+        binding.bottomNavigationView.setOnItemSelectedListener {
+            when(it.itemId) {
+                R.id.recents -> {
+                    frag = RecentsFragment.newInstance()
+                    supportFragmentManager.beginTransaction()
+                        .replace(R.id.container, frag).commit()
+                }
+                R.id.albums -> {
+                    frag = AlbumsFragment.newInstance()
+                    supportFragmentManager.beginTransaction()
+                        .replace(R.id.container, frag).commit()
+                }
+                R.id.artists -> {
+                    frag = ArtistsFragment.newInstance()
+                    supportFragmentManager.beginTransaction()
+                        .replace(R.id.container, frag).commit()
+                }
+                R.id.options -> {
+                    frag = ArtistsFragment.newInstance()
+                    supportFragmentManager.beginTransaction()
+                        .replace(R.id.container, frag).commit()
+                }
+            }
+            true
+        }
     }
 
 }
--- app/src/main/java/br/senac/aula07examples/nv/NavigationViewActivity.kt
+++ app/src/main/java/br/senac/aula07examples/nv/NavigationViewActivity.kt
@@ -1,17 +1,76 @@
 package br.senac.aula07examples.nv
 
 import android.os.Bundle
+import android.view.MenuItem
+import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import br.senac.aula07examples.R
 import br.senac.aula07examples.databinding.ActivityNvBinding
+import br.senac.aula07examples.fragments.AlbumsFragment
+import br.senac.aula07examples.fragments.ArtistsFragment
+import br.senac.aula07examples.fragments.RecentsFragment
 
 class NavigationViewActivity : AppCompatActivity() {
     lateinit var binding: ActivityNvBinding
+    lateinit var toggle: ActionBarDrawerToggle
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityNvBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+        toggle = ActionBarDrawerToggle(
+            this,
+            binding.drawerLayout,
+            R.string.open_menu,
+            R.string.close_menu
+        )
+
+        binding.drawerLayout.addDrawerListener(toggle)
+        toggle.syncState()
+
+        var frag: Fragment = RecentsFragment.newInstance()
+
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.container, frag).commit()
+
+        binding.navigationView.setNavigationItemSelectedListener {
+            when(it.itemId) {
+                R.id.recents -> {
+                    frag = RecentsFragment.newInstance()
+                    supportFragmentManager.beginTransaction()
+                        .replace(R.id.container, frag).commit()
+                }
+                R.id.albums -> {
+                    frag = AlbumsFragment.newInstance()
+                    supportFragmentManager.beginTransaction()
+                        .replace(R.id.container, frag).commit()
+                }
+                R.id.artists -> {
+                    frag = ArtistsFragment.newInstance()
+                    supportFragmentManager.beginTransaction()
+                        .replace(R.id.container, frag).commit()
+                }
+                R.id.options -> {
+                    frag = ArtistsFragment.newInstance()
+                    supportFragmentManager.beginTransaction()
+                        .replace(R.id.container, frag).commit()
+                }
+            }
+
+//            binding.drawerLayout.closeDrawers()
+
+            true
+        }
+
     }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return toggle.onOptionsItemSelected(item)
+    }
+
 
 }
